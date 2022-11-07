998a517740aecdfe452bd1b451395ae9b810fe75
geraldnzeribe <117571342+geraldnzeribe@users.noreply.github.com>
2022-11-07 10:24:55 +0100

    Add files via upload

--- eceassignment.py
+++ eceassignment.py
@@ -1,5 +1,12 @@
 
-print("Hello")
+print("Hello welcome to my form")
+print("please input your details")
+name = input("what is your name :")
+age = input("how old are you:")
+dept = input("what department are you: ")
+print("hello" + " " + name)
+print("you are " + age + "years"+ " "+"old")
+print(" You are in " + dept )
 
 
 
